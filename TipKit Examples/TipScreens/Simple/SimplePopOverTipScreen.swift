ada04f38fda3cea10bfea4ab958ec5f86519bbc9
Jordi Bruin <jordibruin@gmail.com>
2023-08-26 08:35:47 +0200

    Update SimplePopOverTipScreen.swift
    I prefer a symbol because then we can see the changed state (even though it's meaningless in the example app I know 😜 )

--- TipKit Examples/TipScreens/Simple/SimplePopOverTipScreen.swift
+++ TipKit Examples/TipScreens/Simple/SimplePopOverTipScreen.swift
@@ -22,7 +22,7 @@ struct SimplePopOverTipScreen: View {
                 Button(action: {
                     favorited.toggle()
                 }) {
-                    Text("Favorite")
+                    Image(systemName: favorited ? "star.fill" : "star")
                 }
                 .buttonStyle(.plain) // Currently toolbar buttons that want to show a popover tip need to have a style specified. Please see https://developer.apple.com/forums/thread/735961.
                 .popoverTip(SimplePopOverTip(), arrowEdge: .top)
